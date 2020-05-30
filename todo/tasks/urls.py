50537f11d27e20a254d037ce0fad51258135d9aa
Vasav Anandjiwala <vasav.anandjiwala@gmail.com>
2020-05-30 15:06:46 +0530

    Tutorial 3 - Class Based Views related changes

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import task_list,task_detail
+from .views import task_list,task_detail, TaskList
 
 urlpatterns = [
-    path('tasks/', task_list),
+    path('tasks/', TaskList.as_view()),
     path('tasks/<int:id>/', task_detail),
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -3,10 +3,13 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import Task
 from .serializers import TaskSerializer
+from django.http import Http404
 
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework import generics
 
 
 # @csrf_exempt
@@ -59,8 +62,9 @@ from rest_framework.decorators import api_view
 #         return HttpResponse(status=204)
 
 
-###############OPTIMIZED VIEW WITH DRF APIS ###########################################
+###############OPTIMIZED VIEW WITH DRF APIS - Method based ###########################################
 
+#OBSOLETE
 @api_view(['GET','POST'])
 def task_list(request):
     if request.method == 'GET':
@@ -75,6 +79,7 @@ def task_list(request):
             return Response(task.data, status=status.HTTP_201_CREATED)
         return Response(task.errors, status=status.HTTP_400_BAD_REQUEST)
 
+#OBSOLETE
 @api_view(['GET','PUT','DELETE'])
 def task_detail(request, id):
     """
@@ -100,3 +105,59 @@ def task_detail(request, id):
     elif request.method == 'DELETE':
         desired_task.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+###############OPTIMIZED VIEW WITH DRF APIS - Class based ###########################################
+
+# class TaskList(APIView):
+#
+#     def get(self,request):
+#         tasks = Task.objects.all()
+#         serializer = TaskSerializer(tasks, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self,request):
+#         data = JSONParser().parse(request)
+#         task = TaskSerializer(data=data)
+#         if task.is_valid():
+#             task.save()
+#             return Response(task.data, status=status.HTTP_201_CREATED)
+#         return Response(task.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# class TaskDetail(APIView):
+#     def get_task(self,id):
+#         try:
+#             return Task.objects.get(id=id)
+#         except Task.DoesNotExist:
+#             raise Http404
+#
+#     def get(self,request,id):
+#         desired_task = self.get_task(id)
+#         serializer = TaskSerializer(desired_task)
+#         return Response(serializer.data)
+#
+#     def put(self,request,id):
+#         desired_task = self.get_task(id)
+#         data = JSONParser().parse(request)
+#         serializer = TaskSerializer(desired_task, data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self,request,id):
+#         desired_task = self.get_task(id)
+#         desired_task.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+#MIXINs review but not implemented
+###############OPTIMIZED VIEW WITH DRF APIS - Generic class based ###########################################
+
+class TaskList(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
